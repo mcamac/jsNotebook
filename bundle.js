1758f43cf0f1a00f4006129186f12f31ec077e1e
Luis Carli <info@luiscarli.com>
2014-05-10 18:48:53 -0400

    put methods on their on files

--- bundle.js
+++ bundle.js
@@ -9920,6 +9920,9 @@ module.exports = function ctrl_main(){
         initFiles()
         this.render(data)
 
+        d3.select('.blocks')
+            .selectAll('.eval').each(function(){ this.click() })
+
         return main
     }
 
@@ -9990,7 +9993,7 @@ module.exports = function ctrl_main(){
     return main
 }
 
-},{"./dragAndDrop":12,"./emptyNotebookData":13,"./render_block":16,"./render_files":17}],12:[function(require,module,exports){
+},{"./dragAndDrop":12,"./emptyNotebookData":13,"./render_block":18,"./render_files":19}],12:[function(require,module,exports){
 
 module.exports = function dragAndDrop(){
 
@@ -10039,7 +10042,7 @@ module.exports = function dragAndDrop(){
                     dispatch.drop({
                         content: data,
                         name: _.first(file.name.split('.')),
-                        type: 'csv',
+                        type: 'json',
                         size: file.size,
                         date: file.lastModifiedDate
                     })
@@ -10100,8 +10103,179 @@ module.exports = {
     ]
 }
 },{}],14:[function(require,module,exports){
+var render_base = require('./render_base')()
+
+module.exports = function pLine(_opts) {
+    var opts = {
+        data: [1,20,3,40,5],
+        sel: _sel,
+        y: '',
+        x: 'index',
+        values: '',
+        xScale: 'linear',
+        yScale: 'linear',
+        xDomain: 'extent',
+        yDomain: 'extent',
+        height: .5
+    }
+    _.extend(opts, _opts)
+
+    var main = {}
+    _.extend(main, render_base)
+
+    main.render = function(){
+        // wrap data if needed
+        this.data = opts.data
+        if (_.all(this.data, _.isObject) && opts.values == '') {
+            this.data = [this.data]
+        }
+        if (_.all(this.data, _.isNumber)) {
+            this.data = [this.data]
+        }
+
+        // set basics
+        var sel = _sel.append('div')
+
+        var box = {width: 700, height: 350, top:10, left: 10, right:40, bottom: 20}
+        box.height = box.width*opts.height
+
+        this.svgBox(box)
+        this.setSvg(sel)
+
+        // set Accessors
+        this.setAccessors(['x', 'y', 'values'])
+
+        //scales
+        var x = this.createScale('x')
+            .range([0, this.width])
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .ticks(this.width/80, ',.1s')
+            .orient('bottom')
+
+        var y = this.createScale('y')
+            .range([this.height, 0])
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient('right')
+
+        // other
+        line = d3.svg.line()
+            .x(function(d,i){return x(main.acc.x(d,i))})
+            .y(function(d,i){return y(main.acc.y(d,i))})
+
+        //
+        // render
+        //
+        var g = sel.select('g.main')
+        var gLines = g.append('g').classed('lines', true)
+            .selectAll('path').data(this.data)
+        gLines.enter().append('path')
+            .attr('d', function(d,i){
+                return line(main.acc.values(d))
+            })
+            .style({
+                fill: 'none',
+                stroke: 'black'
+            })
+
+        // axis
+        g.append('g')
+            .classed('axis', true)
+            .attr('transform', 'translate(0,'+this.height+')')
+            .call(xAxis)
+        g.append('g')
+            .classed('axis', true)
+            .attr('transform', 'translate('+this.width+',0)')
+            .call(yAxis)
+
+        // labels
+        g.append('text')
+            .classed('label', true)
+            .attr('transform', 'translate(0,'+(this.height-5)+')')
+            .text(function () {
+                if (opts.xName) return opts.xName
+                if (_.isString(opts.x)) return opts.x
+                return ''
+            })
+        g.append('text')
+            .classed('label', true)
+            .attr({
+                'transform': 'translate('+(this.width-7)+','+(this.height-5)+') rotate(-90)',
+                'text-anchor': 'start'
+            })
+            .text(function () {
+                if (opts.yName) return opts.yName
+                if (_.isString(opts.y)) return opts.y
+                return ''
+            })
+
+    }
+
+    main.getDomain = function (domainType, _acc){
+        var that = this
+        var max = d3.max(this.data, function(d,i){
+            return d3.max(main.acc.values(d), _acc)
+        })
+        var min = d3.min(this.data, function(d,i){
+            return d3.min(main.acc.values(d), _acc)
+        })
+        if (domainType == 'zero') {
+            return [d3.min([0,min]), d3.max([0, max])]
+        }
+        if (domainType == 'extent') {
+            return [min, max]
+        }
+        print('invalid domainType')
+    }
+
+    main.setAccessors = function (accNames) {
+        this.acc = {}
+        _.each(accNames, function(d, i){
+            var value = main.acc[d] = opts[d]
+            if (_.isFunction(value)) return
+            if (value == 'index') {
+                main.acc[d] = function(d,i){return i}
+                return
+            }
+            if (_.isString(value) || _.isNull(value)) return main.acc[d] = main.accessorFromString(value)
+        })
+    }
+
+    main.accessorFromString = function (string){
+        var identityAcc = function(d){ return d }
+        if (string == '') return identityAcc
+        if (_.isNull(string)) return identityAcc
+        if (string == 'index') return function(d,i){return i}
+        var path = string.split('.')
+        return _.reduce(path, function(acc, string){
+            return function(d){
+                return acc(d)[string]
+            }
+        }, identityAcc)
+    }
+
+    main.createScale = function (name) {
+        return d3.scale[opts[name+'Scale']]()
+            .domain(this.getDomain(opts[name+'Domain'], this.acc[name]))
+    }
+
+    main.createScalesAndAxis = function (name){
+        this[name] = d3.scale[opts[name+'Scale']]()
+            // .range([0, this.width])
+            .domain(this.getDomain(opts[name+'Domain'], this.acc[name]))
+        this[name+'Axis'] = d3.svg.axis()
+            .scale(this[name])
+    }
+
+    main.render()
+
+}
+},{"./render_base":17}],15:[function(require,module,exports){
+
 var ctrlMain = require('./ctrl_main')()
 var emptyData = require('./emptyNotebookData')
+require('./methods')
 
 if (localStorage.notebook) {
     emptyData = JSON.parse(localStorage.notebook, dateReviver)
@@ -10114,8 +10288,9 @@ if (localStorage.notebook) {
 }
 
 ctrlMain.init(emptyData)
-
-//////////
+},{"./ctrl_main":11,"./emptyNotebookData":13,"./methods":16}],16:[function(require,module,exports){
+// methods
+var pLine = require('./lc_pLine')
 
 var methods = {
     print: function(string, sel){
@@ -10144,181 +10319,9 @@ var methods = {
         pLine: pLine
     }
 }
+
 _.extend(window, methods)
-
-
-var render_base = require('./render_base')()
-
-module.exports = function render_vis(){
-    var main = {}
-    _.extend(main, render_base)
-    main.render = function(sel, data) {
-
-        return main
-    }
-    return main
-}
-
-////////////////////
-// super pLine plot
-
-function pLine(_opts) {
-    var opts = {
-        d: [1,20,3,40,5],
-        sel: _sel,
-        y: '',
-        x: function(d,i){return i},
-        values: '',
-        xScale: 'linear',
-        yScale: 'linear',
-        xDomain: 'extent',
-        yDomain: 'extent',
-        h: .5
-    }
-    _.extend(opts, _opts)
-
-    var main = {}
-    _.extend(main, render_base)
-
-    main.render = function(){
-        // wrap data if needed
-        var data = opts.d
-        if (_.all(data, _.isObject) && opts.values == '') {
-            data = [data]
-        }
-        if (_.all(data, _.isNumber)) {
-            data = [data]
-        }
-
-
-        // set basics
-        var sel = _sel.append('div')
-
-        var box = {width: 700, height: 350, top:10, left: 10, right:40, bottom: 20}
-        box.height = box.width*opts.h
-
-        this.svgBox(box)
-        this.setSvg(sel)
-
-        // set Accessors
-        var acc = {
-            x: opts.x,
-            y: opts.y,
-            values: opts.values
-        }
-        _.each(acc, function(d, i, array){
-            if (_.isFunction(d)) return
-            if (d == '_index') {
-                array[i] = function(d,i){return i}
-                return
-            }
-            if (_.isString(d) || _.isNull(d)) return array[i] = main.accFromString(d)
-        })
-        //scales
-        x = d3.scale[opts.xScale]()
-            .range([0, this.width])
-            .domain(getDomain(opts.xDomain, acc.x))
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .ticks(main.width/80, ',.1s')
-            .orient('bottom')
-
-        y = d3.scale[opts.yScale]()
-            .range([this.height, 0])
-            .domain(getDomain(opts.yDomain, acc.y))
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            // .ticks(main.width/80, ',.1s')
-            .orient('right')
-
-        // other
-        line = d3.svg.line()
-            .x(function(d,i){return x(acc.x(d,i))})
-            .y(function(d,i){return y(acc.y(d,i))})
-
-        //
-        // render
-        //
-        var g = sel.select('g.main')
-        var gLines = g.append('g').classed('lines', true)
-            .selectAll('path').data(data)
-        gLines.enter().append('path')
-            .attr('d', function(d,i){
-                return line(acc.values(d))
-            })
-            .style({
-                fill: 'none',
-                stroke: 'black'
-            })
-
-        // axis
-        g.append('g')
-            .classed('axis', true)
-            .attr('transform', 'translate(0,'+main.height+')')
-            .call(xAxis)
-        g.append('g')
-            .classed('axis', true)
-            .attr('transform', 'translate('+main.width+',0)')
-            .call(yAxis)
-
-        // labels
-        g.append('text')
-            .classed('label', true)
-            .attr('transform', 'translate(0,'+(main.height-5)+')')
-            .text(function () {
-                if (opts.xName) return opts.xName
-                if (_.isString(opts.x)) return opts.x
-                return ''
-            })
-        g.append('text')
-            .classed('label', true)
-            .attr({
-                'transform': 'translate('+(main.width-7)+','+(main.height-5)+') rotate(-90)',
-                'text-anchor': 'start'
-            })
-            .text(function () {
-                if (opts.yName) return opts.yName
-                if (_.isString(opts.y)) return opts.y
-                return ''
-            })
-
-        function getDomain(domainType, _acc){
-            var max = d3.max(data, function(d,i){
-                return d3.max(acc.values(d), _acc)
-            })
-            var min = d3.min(data, function(d,i){
-                return d3.min(acc.values(d), _acc)
-            })
-            if (domainType == 'zero') {
-                return [d3.min([0,min]), d3.max([0, max])]
-            }
-            if (domainType == 'extent') {
-                return [min, max]
-            }
-            print('invalid domainType')
-        }
-
-    }
-
-    main.accFromString = function (string){
-        var identityAcc = function(d){ return d }
-        if (string == '') return identityAcc
-        if (_.isNull(string)) return identityAcc
-        if (string == 'index') return function(d,i){return i}
-        var path = string.split('.')
-        return _.reduce(path, function(acc, string){
-            return function(d){
-                return acc(d)[string]
-            }
-        }, identityAcc)
-    }
-
-    main.render()
-
-}
-
-
-},{"./ctrl_main":11,"./emptyNotebookData":13,"./render_base":15}],15:[function(require,module,exports){
+},{"./lc_pLine":14}],17:[function(require,module,exports){
 
 module.exports = function render_base(){
 
@@ -10419,7 +10422,7 @@ module.exports = function render_vis(){
 }
 
 */
-},{}],16:[function(require,module,exports){
+},{}],18:[function(require,module,exports){
 var CodeMirror = require('../bower_components/codemirror/lib/codemirror.js')
 require('../bower_components/codemirror/mode/javascript/javascript.js')
 require('../bower_components/codemirror/keymap/sublime.js')
@@ -10573,7 +10576,7 @@ module.exports = function render_block(){
 
     return main
 }
-},{"../bower_components/codemirror/addon/comment/comment.js":1,"../bower_components/codemirror/addon/edit/closebrackets.js":2,"../bower_components/codemirror/addon/edit/matchbrackets.js":3,"../bower_components/codemirror/addon/hint/javascript-hint.js":4,"../bower_components/codemirror/addon/hint/show-hint.js":5,"../bower_components/codemirror/addon/selection/active-line.js":7,"../bower_components/codemirror/keymap/sublime.js":8,"../bower_components/codemirror/lib/codemirror.js":9,"../bower_components/codemirror/mode/javascript/javascript.js":10}],17:[function(require,module,exports){
+},{"../bower_components/codemirror/addon/comment/comment.js":1,"../bower_components/codemirror/addon/edit/closebrackets.js":2,"../bower_components/codemirror/addon/edit/matchbrackets.js":3,"../bower_components/codemirror/addon/hint/javascript-hint.js":4,"../bower_components/codemirror/addon/hint/show-hint.js":5,"../bower_components/codemirror/addon/selection/active-line.js":7,"../bower_components/codemirror/keymap/sublime.js":8,"../bower_components/codemirror/lib/codemirror.js":9,"../bower_components/codemirror/mode/javascript/javascript.js":10}],19:[function(require,module,exports){
 module.exports = function render_files(){
 
     var dispatch = d3.dispatch('delete')
@@ -10661,4 +10664,4 @@ module.exports = function render_files(){
     d3.rebind(main, dispatch, 'on')
     return main
 }
-},{}]},{},[14])
+},{}]},{},[15])
